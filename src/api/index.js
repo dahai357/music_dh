43701f744aec70931fbce40fdbe393f3b2295b0f
wangyang <da_dahai@163.com>
2020-01-09 17:30:32 +0800

    什么都没改

--- src/api/index.js
+++ src/api/index.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 //请求路径
 axios.defaults.baseURL ="http://118.31.23.45:80"
 //请求头
-axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8'
+// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8'
 export default {
 //接口地址 : /banner
 //调用例子 : /banner, /banner?type=2
